715267191f2bb39ba5fce960d6e6eedd5efaca55
anayabanerjee2208 <anaya.banerjee@gmail.com>
2026-05-26 23:32:31 +0100

    commit

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Http.Json;
 using VoiceBotNew.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-var endpoint = builder.Configuration["Copilot:Endpoint"];
-builder.Services.AddSingleton(new BotConfig { Endpoint = endpoint });
-
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Load appsettings.json manually (required in WASM)
+var http = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+var config = await http.GetFromJsonAsync<BotConfig>("appsettings.json");
+
+// Register BotConfig so you can inject it
+builder.Services.AddSingleton(config);
+
+// HttpClient for your app
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
